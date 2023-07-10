eadf7647ccd0c5d71b1f843b1d5ccbfb73eae7a8
Leandro Portugal <leandroportugal@outlook.com.br>
2023-07-09 21:38:02 -0300

    Add initial variables and DOM element selections, and initialize timer values and state

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,35 @@
-const circleElement = document.querySelector('circle');
+const circleElement = document.querySelector('.circle');
+const timeElement = document.querySelector('.time');
+const timeModeElement = document.querySelector('.time-mode');
+const turnsElement = document.querySelector('.turns');
+const controlButton = document.querySelector('.timer-control');
+const resetButton = document.querySelector('.reset-timer')
+const notificationSound = document.querySelector('#notification');
+
+let isRunning,
+    isBreakTime,
+    workTime,
+    breakTime,
+    longBreakTime,
+    totalTurns,
+    currentTurn,
+    totalTime,
+    timeRemaining,
+    time;
+
+function startValues(){
+    
+    isRunning,
+    isBreakTime,
+    workTime = 1500,
+    breakTime = 300,
+    longBreakTime = 900,
+    totalTurns,
+    currentTurn,
+    totalTime,
+    timeRemaining,
+    time;
+}
 
 function setCirclePercent(percent){
 
